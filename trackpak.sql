c01f53166109e1da5a11d8d584296209be60deea
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2023-10-04 19:11:13 -0400

    Update trackpak.sql

--- trackpak.sql
+++ trackpak.sql
@@ -40,16 +40,19 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Volcando datos para la tabla trackpak.migrations: ~6 rows (aproximadamente)
+-- Volcando datos para la tabla trackpak.migrations: ~8 rows (aproximadamente)
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(1, '2014_10_12_000000_create_users_table', 1),
 	(2, '2014_10_12_100000_create_password_reset_tokens_table', 1),
 	(3, '2019_08_19_000000_create_failed_jobs_table', 1),
 	(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 	(5, '2023_10_02_205137_create_packages_table', 2),
-	(6, '2023_10_02_211831_create_packages_table', 3);
+	(6, '2023_10_02_211831_create_packages_table', 3),
+	(7, '2023_10_03_134847_create_packagesr_table', 4),
+	(8, '2023_10_03_140525_create_pcertificate_table', 5),
+	(9, '2023_10_03_141018_create_pcertificates_table', 6);
 
 -- Volcando estructura para tabla trackpak.packages
 CREATE TABLE IF NOT EXISTS `packages` (
@@ -67,11 +70,11 @@ CREATE TABLE IF NOT EXISTS `packages` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Volcando datos para la tabla trackpak.packages: ~1 rows (aproximadamente)
 INSERT INTO `packages` (`id`, `CODIGO`, `DESTINATARIO`, `TELEFONO`, `PAIS`, `CUIDAD`, `ZONA`, `VENTANILLA`, `PESO`, `TIPO`, `ESTADO`, `created_at`, `updated_at`) VALUES
-	(1, 'UV643247440UZ', 'FLAVIA JHULIANA GONZALES ROCA', '71495885', 'UZ', 'LA PAZ', 'DND', '33', 0.03, 'SOBRE', 'ALMACEN', NULL, NULL);
+	(2, 'UV643247440UZ', 'FLAVIA JHULIANA GONZALES ROCA', '71495885', 'UZ', 'LA PAZ', 'DND', '33', 0.03, 'PAQUETE', 'ALMACEN', '2023-10-03 18:39:46', '2023-10-04 03:02:30');
 
 -- Volcando estructura para tabla trackpak.password_reset_tokens
 CREATE TABLE IF NOT EXISTS `password_reset_tokens` (
@@ -82,6 +85,29 @@ CREATE TABLE IF NOT EXISTS `password_reset_tokens` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- Volcando datos para la tabla trackpak.password_reset_tokens: ~0 rows (aproximadamente)
+
+-- Volcando estructura para tabla trackpak.pcertificates
+CREATE TABLE IF NOT EXISTS `pcertificates` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `CODIGO` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `DESTINATARIO` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `DIRECCION` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `TELEFONO` int NOT NULL,
+  `PAIS` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `CUIDAD` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ZONA` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `VENTANILLA` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `PESO` double(8,2) NOT NULL,
+  `TIPO` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ESTADO` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla trackpak.pcertificates: ~0 rows (aproximadamente)
+INSERT INTO `pcertificates` (`id`, `CODIGO`, `DESTINATARIO`, `DIRECCION`, `TELEFONO`, `PAIS`, `CUIDAD`, `ZONA`, `VENTANILLA`, `PESO`, `TIPO`, `ESTADO`, `created_at`, `updated_at`) VALUES
+	(1, 'RR935638274ES', 'ARCOS VASQUEZ ANTONIO', '--', 0, 'ES', 'COCHABAMBA', '--', 'UNICA', 0.00, 'PAQUETE', 'ALMACEN', '2023-10-03 18:25:22', '2023-10-04 03:02:16');
 
 -- Volcando estructura para tabla trackpak.personal_access_tokens
 CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
@@ -116,9 +142,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `users_email_unique` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- Volcando datos para la tabla trackpak.users: ~1 rows (aproximadamente)
+-- Volcando datos para la tabla trackpak.users: ~0 rows (aproximadamente)
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-	(1, 'Marco Espinoza', 'marco@gmail.com', NULL, '$2y$10$ogrSrJRSIDk6DyyFsxMKau6TIuEHYrrK3C.u.tRJd4Qyr7HeV3VfK', NULL, '2023-10-02 23:45:13', '2023-10-02 23:45:13');
+	(1, 'Marco Espinoza', 'marco@gmail.com', NULL, '$2y$10$ogrSrJRSIDk6DyyFsxMKau6TIuEHYrrK3C.u.tRJd4Qyr7HeV3VfK', 'uNuV8V4fi6PNjhSmpmL7RBcjq3yPn2arITg2K9DNOpbRqYdJlu9ayRDLiNki', '2023-10-02 23:45:13', '2023-10-02 23:45:13');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
