12a8b0440ee7c62c10d917546d8bf743b3fa8efd
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2023-11-22 19:16:43 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -124,7 +124,7 @@
                     <br />
                     <ul class="text-black font-bold">
                         <li class="mb-2">
-                            - Fotocopia de Carnet de identidad.
+                            - Fotocopia de CI.
                         </li>
                         <li class="mb-2">
                             - Paquete abierto con el embalaje nesesario.
@@ -161,7 +161,7 @@
                         <br />
                         <ul class="text-black font-bold">
                             <li class="mb-2">
-                                - Carnet de identidad / Pasaporte.
+                                - CI / Pasaporte.
                             </li>
                         </ul>
                         </p>
@@ -264,7 +264,7 @@
                 <div class="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
                     <div class="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
                         <div class="p-8 text-3xl font-bold text-center border-b-4">
-                            Artículo u Objetos Volátiles Peligrosos
+                            Artículos u Objetos Volátiles Peligrosos
                         </div>
                         <ul class="w-full text-center text-sm">
                             <li class="border-b py-4">EXPLOSIVOS</li>
@@ -278,14 +278,14 @@
                     <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
                         <div class="w-full pt-6 text-3xl text-gray-600 font-bold text-center">
                             <div>NO</div>
-                            <div class="text-base">Artículo u Objetos Peligrosos</div>
+                            <div class="text-base">Artículos u Objetos Peligrosos</div>
                         </div>
                     </div>
                 </div>
                 <div
                     class="flex flex-col w-5/6 lg:w-1/3 mx-auto lg:mx-0 rounded-lg bg-white mt-4 sm:-mt-6 shadow-lg z-10">
                     <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-                        <div class="w-full p-8 text-3xl font-bold text-center">Artículo u Objetos Permitida</div>
+                        <div class="w-full p-8 text-3xl font-bold text-center">Artículos u Objetos Permitidos</div>
                         <div class="h-1 w-full gradient my-0 py-0 rounded-t"></div>
                         <ul class="w-full text-center text-base font-bold">
                             <li class="border-b py-4">ROPA</li>
@@ -299,7 +299,7 @@
                     <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
                         <div class="w-full pt-6 text-3xl text-gray-600 font-bold text-center">
                             <div>SI</div>
-                            <div class="text-base">Artículo u Objetos Permitidos</div>
+                            <div class="text-base">Artículos u Objetos Permitidos</div>
                         </div>
                         <div class="flex items-center justify-center">
                             <a href="https://www.upu.int/UPU/media/upu/files/UPU/outreachAndCampaigns/Dangerous%20Goods%20Campaign/UPU_Flyer_es_web.pdf"
@@ -312,7 +312,7 @@
                 <div class="flex flex-col w-5/6 lg:w-1/4 mx-auto lg:mx-0 rounded-none lg:rounded-l-lg bg-white mt-4">
                     <div class="flex-1 bg-white text-gray-600 rounded-t rounded-b-none overflow-hidden shadow">
                         <div class="p-8 text-3xl font-bold text-center border-b-4">
-                            Artículo u Objetos Peligrosa Prohibida
+                            Artículos u Objetos Peligrosa Prohibidos
                         </div>
                         <ul class="w-full text-center text-sm">
                             <li class="border-b py-4">MEDICAMENTOS</li>
@@ -326,7 +326,7 @@
                     <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
                         <div class="w-full pt-6 text-3xl text-gray-600 font-bold text-center">
                             <div>NO</div>
-                            <div class="text-base">Artículo u Objetos Prohibidos</div>
+                            <div class="text-base">Artículos u Objetos Prohibidos</div>
                         </div>
                     </div>
                 </div>
