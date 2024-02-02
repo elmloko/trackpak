23737f4666792bd38ada7815eabb928f3e6b8a54
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-02-02 11:43:59 -0400

    Update ClasificacionPackages.php

--- app/Livewire/ClasificacionPackages.php
+++ app/Livewire/ClasificacionPackages.php
@@ -72,31 +72,56 @@ class ClasificacionPackages extends Component
                 $query->where('CUIDAD', $this->selectedCity);
             })
             ->get();
+        
         // Contar la cantidad de paquetes seleccionados
         $cantidadPaquetes = count($paquetesSeleccionados);
+        
         // Obtener la ciudad del primer paquete (si existe)
         $ciudadPaquete = $paquetesSeleccionados->isNotEmpty() ? $paquetesSeleccionados->first()->CUIDAD : null;
-        foreach ($paquetesSeleccionados as $paquete) {
+        
+        // Obtener el último número de despacho para la ciudad seleccionada
+        $ultimoNumeroDespacho = Bag::where('OFDESTINO', $ciudadPaquete)->max('NRODESPACHO');
+        $ultimoNumeroDespacho = $ultimoNumeroDespacho ? $ultimoNumeroDespacho : 0;
+        
+        // Incrementar el número de despacho para generar el nuevo número correlativo
+        $nuevoNumeroDespacho = (int)$ultimoNumeroDespacho + 1;
+        $nuevoNumeroDespachoFormatado = str_pad($nuevoNumeroDespacho, 4, '0', STR_PAD_LEFT);
+        
+        foreach ($paquetesSeleccionados as $indice => $paquete) {
+            $numeroSaca = $indice + 1;
+            $numeroSacaFormatado = str_pad($numeroSaca, 4, '0', STR_PAD_LEFT);
+            
+            // Generar el código con el formato solicitado
+            $codigoDespacho = $nuevoNumeroDespachoFormatado . '/' . $numeroSacaFormatado;
+    
             if ($paquete) {
                 $paquete->ESTADO = 'DESPACHO';
                 $paquete->datedespachoclasificacion = now()->toDateTimeString();
                 $paquete->save();
             }
+    
+            // Agregar lógica para el evento y cualquier otra acción que necesites
             Event::create([
                 'action' => 'DESPACHO',
                 'descripcion' => 'Destino de Clasificacion hacia Ventanilla',
                 'user_id' => auth()->user()->id,
-                'codigo' => $paquete->CODIGO,
+                'codigo' => $codigoDespacho, // Utiliza el nuevo código generado
             ]);
+    
+            // Incrementar el número de saca
+            $nuevoNumeroDespacho++;
         }
+    
+        // Actualizar la base de datos con el nuevo número de despacho para la ciudad
         Bag::create([
             'OFDESTINO' => $ciudadPaquete,
             'PAQUETES' => $cantidadPaquetes,
-            'NRODESPACHO' => 'TuValorAqui',
+            'NRODESPACHO' => $nuevoNumeroDespachoFormatado,
             'OFCAMBIO' => auth()->user()->Regional,
             'ESTADO' => 'APERTURA',
             'ano_creacion' => now()->year,
         ]);
+    
         // Restablecer la selección
         $this->resetSeleccion();
     
@@ -104,12 +129,12 @@ class ClasificacionPackages extends Component
         $pdf = PDF::loadView('package.pdf.despachopdf', ['packages' => $paquetesSeleccionados]);
         // Obtener el contenido del PDF
         $pdfContent = $pdf->output();
-        
+    
         // Generar una respuesta con el contenido del PDF para descargar
         return response()->streamDownload(function () use ($pdfContent) {
             echo $pdfContent;
         }, 'Despacho_Clasificacion.pdf');
-    }
+    }    
 
     private function getPackageIds()
     {
