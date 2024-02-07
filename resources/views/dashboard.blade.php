b7791a808a3e7853574e0df5092a030185204b0d
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-02-07 18:01:33 -0400

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Seguimiento de Paqueteria Nacional de Agencia Boliviana de Correos</h1>
+    <h1>Sistema de Paquetes Ordinario/ Casillas Postales/ Envios de Correspondencia Agrupada/ Encomiendas Nacionales de la Agencia Boliviana de Correos</h1>
 @stop
 
 @section('content')
