55176e61711583de81a8f3d4d639d8072b776712
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-06-25 09:26:38 -0400

    Update cn22.blade.php

--- resources/views/national/pdf/cn22.blade.php
+++ resources/views/national/pdf/cn22.blade.php
@@ -40,33 +40,11 @@
         }
 
         .rotated-table {
-            transform: rotate(360deg);
-            transform-origin: bottom top;
-        }
-
-        /* Estilos específicos para la impresión */
-        @media print {
-            body {
-                margin: 10px;
-            }
-
-            table {
-                font-size: 6px;
-            }
-
-            .rotated-table-wrapper {
-                display: block;
-                position: relative;
-                height: 300px; /* Ajusta la altura según sea necesario */
-            }
-
-            .rotated-table {
-                transform: rotate(90deg);
+            transform: rotate(90deg);
                 transform-origin: left top;
                 position: absolute;
-                top: 50px; /* Ajusta la posición según sea necesario */
-                left: -50px; /* Ajusta la posición según sea necesario */
-            }
+                bottom: 50px; /* Ajusta la posición según sea necesario */
+                right: 0px; /* Ajusta la posición según sea necesario */
         }
     </style>
 </head>
@@ -90,7 +68,7 @@
                     <td colspan="3"><img src="{{ public_path('images/images.png') }}" alt="" width="150" height="50"></td>
                     <td colspan="3" rowspan="2">
                         <div style="text-align: center; font-size: 14px;">
-                            {!! DNS1D::getBarcodeHTML($national->CODIGO, 'C128', 1.25, 50) !!}<br>{{ $national->CODIGO }}
+                            {!! DNS1D::getBarcodeHTML($national->CODIGO, 'C128', 1.30, 40) !!}<br>{{ $national->CODIGO }}
                         </div>
                     </td>
                     <td rowspan="8" style="text-align: center; font-size: 7px;">
@@ -147,8 +125,83 @@
     </div>
 
     <!-- Añadimos un margen grande entre las tablas repetidas -->
-    <div style="height: 20px;"></div>
+    <div style="height: 40px;"></div>
 
+
+
+    <div class="page-break">
+        <table style="width: 700px;">
+            <colgroup>
+                <col style="width: 87px">
+                <col style="width: 90px">
+                <col style="width: 80px">
+                <col style="width: 86px">
+                <col style="width: 108px">
+                <col style="width: 138px">
+                <col style="width: 20px">
+                <col style="width: 125px">
+            </colgroup>
+            <thead>
+                <tr>
+                    <td colspan="3"><img src="{{ public_path('images/images.png') }}" alt="" width="150" height="50"></td>
+                    <td colspan="3" rowspan="2">
+                        <div style="text-align: center; font-size: 14px;">
+                            {!! DNS1D::getBarcodeHTML($national->CODIGO, 'C128', 1.30, 40) !!}<br>{{ $national->CODIGO }}
+                        </div>
+                    </td>
+                    <td rowspan="8" style="text-align: center; font-size: 7px;">
+                        {{-- Datos de ubicación --}}
+                    </td>
+                </tr>
+                <tr>
+                    <td>OF. ORIGEN: <br>
+                        <div style="text-align: right;">{{ $national->ORIGEN }}</div>
+                    </td>
+                    <td>OF. DESTINO: <br>
+                        <div style="text-align: right;">{{ $national->DESTINO }}</div>
+                    </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td colspan="3" rowspan="2">
+                        NOMBRE REMITENTE: <br>
+                        <div style="text-align: right; font-size: 14px;">{{ $national->NOMBRESREMITENTE }}</div>
+                    </td>
+                    <td colspan="3" rowspan="2">NOMBRE DESTINATARIO: <br>
+                        <div style="text-align: right; font-size: 14px;">{{ $national->NOMBRESDESTINATARIO }}</div>
+                    </td>
+                </tr>
+                <tr></tr>
+                <tr>
+                    <td colspan="3" rowspan="2">DIRECCION Y TELEFONO:
+                        <div style="text-align: right; font-size: 14px;"><br>{{ $national->TELEFONOREMITENTE }}</div>
+                    </td>
+                    <td colspan="3" rowspan="2">DIRECCION Y TELEFONO:<br>
+                        <div style="text-align: right; font-size: 14px;">
+                            {{ $national->DIRECCION }}<br>{{ $national->TELEFONOREMITENTE }}</div>
+                    </td>
+                </tr>
+                </tr>
+                <tr></tr>
+                <tr>
+                    <td colspan="3">DESCRIPCIÓN:</td>
+                    <td rowspan="2" style="vertical-align: top;">FIRMA AGBC:<br></td>
+                    <td colspan="2" rowspan="2" style="vertical-align: top;">FIRMA REMITENTE:<br></td>
+                </tr>
+                <tr>
+                    <td>IMPORTE: <br>
+                        <div style="text-align: right;">{{ $national->IMPORTE }}</div>
+                    </td>
+                    <td>PESO:<br>
+                        <div style="text-align: right;">{{ $national->PESO }}</div>
+                    </td>
+                    <td>FECHA:<br>
+                        <div style="text-align: right;">{{ $national->created_at }}</div>
+                    </td>
+                </tr>
+            </thead>
+        </table>
+    </div>
     <div class="rotated-table-wrapper">
         <div class="rotated-table">
             <table style="width: 300px;">
@@ -160,7 +213,7 @@
                 </colgroup>
                 <thead>
                     <tr>
-                        <th colspan="3" rowspan="4">{!! DNS1D::getBarcodeHTML($national->CODIGO, 'C128', 1.25, 50) !!}<br>{{ $national->CODIGO }}</th>
+                        <th colspan="3" rowspan="4">{!! DNS1D::getBarcodeHTML($national->CODIGO, 'C128', 1.30, 40) !!}<br>{{ $national->CODIGO }}</th>
                         <th>RETORNAR A:</th>
                     </tr>
                     <tr>
@@ -233,80 +286,6 @@
                 </tbody>
             </table>
         </div>
-    </div>
-
-    <div class="page-break">
-        <table style="width: 700px;">
-            <colgroup>
-                <col style="width: 87px">
-                <col style="width: 90px">
-                <col style="width: 80px">
-                <col style="width: 86px">
-                <col style="width: 108px">
-                <col style="width: 138px">
-                <col style="width: 20px">
-                <col style="width: 125px">
-            </colgroup>
-            <thead>
-                <tr>
-                    <td colspan="3"><img src="{{ public_path('images/images.png') }}" alt="" width="150" height="50"></td>
-                    <td colspan="3" rowspan="2">
-                        <div style="text-align: center; font-size: 14px;">
-                            {!! DNS1D::getBarcodeHTML($national->CODIGO, 'C128', 1.25, 50) !!}<br>{{ $national->CODIGO }}
-                        </div>
-                    </td>
-                    <td rowspan="8" style="text-align: center; font-size: 7px;">
-                        {{-- Datos de ubicación --}}
-                    </td>
-                </tr>
-                <tr>
-                    <td>OF. ORIGEN: <br>
-                        <div style="text-align: right;">{{ $national->ORIGEN }}</div>
-                    </td>
-                    <td>OF. DESTINO: <br>
-                        <div style="text-align: right;">{{ $national->DESTINO }}</div>
-                    </td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td colspan="3" rowspan="2">
-                        NOMBRE REMITENTE: <br>
-                        <div style="text-align: right; font-size: 14px;">{{ $national->NOMBRESREMITENTE }}</div>
-                    </td>
-                    <td colspan="3" rowspan="2">NOMBRE DESTINATARIO: <br>
-                        <div style="text-align: right; font-size: 14px;">{{ $national->NOMBRESDESTINATARIO }}</div>
-                    </td>
-                </tr>
-                <tr></tr>
-                <tr>
-                    <td colspan="3" rowspan="2">DIRECCION Y TELEFONO:
-                        <div style="text-align: right; font-size: 14px;"><br>{{ $national->TELEFONOREMITENTE }}</div>
-                    </td>
-                    <td colspan="3" rowspan="2">DIRECCION Y TELEFONO:<br>
-                        <div style="text-align: right; font-size: 14px;">
-                            {{ $national->DIRECCION }}<br>{{ $national->TELEFONOREMITENTE }}</div>
-                    </td>
-                </tr>
-                </tr>
-                <tr></tr>
-                <tr>
-                    <td colspan="3">DESCRIPCIÓN:</td>
-                    <td rowspan="2" style="vertical-align: top;">FIRMA AGBC:<br></td>
-                    <td colspan="2" rowspan="2" style="vertical-align: top;">FIRMA REMITENTE:<br></td>
-                </tr>
-                <tr>
-                    <td>IMPORTE: <br>
-                        <div style="text-align: right;">{{ $national->IMPORTE }}</div>
-                    </td>
-                    <td>PESO:<br>
-                        <div style="text-align: right;">{{ $national->PESO }}</div>
-                    </td>
-                    <td>FECHA:<br>
-                        <div style="text-align: right;">{{ $national->created_at }}</div>
-                    </td>
-                </tr>
-            </thead>
-        </table>
     </div>
 </body>
 
