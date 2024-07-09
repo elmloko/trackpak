9263b4aa8ca7ac9fea1251aeef4de105d2a224b5
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-07-09 15:34:13 -0400

    Update cn38.blade.php

--- resources/views/bag/pdf/cn38.blade.php
+++ resources/views/bag/pdf/cn38.blade.php
@@ -11,7 +11,6 @@
             width: 100%;
             border-collapse: collapse;
             font-size: 12px;
-
         }
 
         th,
@@ -67,177 +66,149 @@
             font-weight: normal;
             line-height: 0.1;
         }
+
+        /* Estilos adicionales para posicionar la firma y la tabla al final */
+        body {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+
+        .content {
+            flex: 1;
+        }
+
+        .footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+        }
     </style>
 </head>
 
 <body>
-    <div class="header">
-        <div class="logo">
-            <img src="{{ public_path('images/images.png') }}" alt="" width="150" height="50">
-            <p class="cn">CN 38</p>
+    <div class="content">
+        <div class="header">
+            <div class="logo">
+                <img src="{{ public_path('images/images.png') }}" alt="" width="150" height="50">
+                <p class="cn">CN 38</p>
+            </div>
+            <div class="title">
+                <h2>Factura de Entrega</h2>
+                <h3>AGENCIA BOLIVIANA DE CORREOS</h3>
+            </div>
         </div>
-        <div class="title">
-            <h2>Factura de Entrega</h2>
-            <h3>AGENCIA BOLIVIANA DE CORREOS</h3>
-        </div>
-    </div>
-    <table>
-        <tbody>
-            <tr>
-                <th style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
-                    @foreach ($bags->take(1) as $bag)
-                        Oficina de Cambio:{{ $bag->OFCAMBIO }} - {{ $bag->OFCAM108 }} / Oficina de
-                        Destino:{{ $bag->OFDESTINO }} - {{ $bag->OFDES108 }}
-                    @endforeach
-                </th>
-                <th style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
-                    @foreach ($bags->take(1) as $bag)
-                        Medio de Trasporte:{{ $bag->ITINERARIO }} = {{ $bag->TRASPORTE }}
-                    @endforeach
-                </th>
-            </tr>
-            <tr>
-                <td style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
-                    <p>Fecha: {{ now()->format('Y-m-d H:i') }}</p>
-                </td>
-                <td style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
-                    @foreach ($bags->take(1) as $bag)
-                        Observaciones:{{ $bag->OBSERVACIONES }}
-                    @endforeach
-                </td>
-            </tr>
-        </tbody>
-    </table>
-    <table class="first-table">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Despacho</th>
-                <th>Origen</th>
-                <th>Destino</th>
-                <th>Correspondencia</th>
-                <th>Peso (Kg.)</th>
-                <th>Observaciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php $i = 1; @endphp <!-- Inicializa $i con 1 -->
-            @foreach ($bags as $bag)
+        <table>
+            <tbody>
                 <tr>
-                    <td>{{ $i }}</td>
-                    <td>{{ $bag->NRODESPACHO }} / {{ $bag->NROSACA }}{{ $bag->FIN == 'F' ? 'F' : '' }}</td>
-                    <td>{{ $bag->OFCAMBIO }}</td>
-                    <td>{{ $bag->OFDESTINO }}</td>
-                    <td>
-                        @switch($bag->TIPO)
-                            @case('U')
-                                1 ORDINARIO
-                            @break
-
-                            @case('M')
-                                1 SACA M
-                            @break
-
-                            @case('R')
-                                1 CERTIFICADO
-                            @break
-
-                            @default
-                                Desconocido
-                        @endswitch
-                    </td>
-                    <td>{{ $bag->PESOF }}</td>
-                    <td>{{ $bag->OBSERVACIONES }}</td>
+                    <th style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
+                        @foreach ($bags->take(1) as $bag)
+                            Oficina de Cambio:{{ $bag->OFCAMBIO }} - {{ $bag->OFCAM108 }} / Oficina de
+                            Destino:{{ $bag->OFDESTINO }} - {{ $bag->OFDES108 }}
+                        @endforeach
+                    </th>
+                    <th style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
+                        @foreach ($bags->take(1) as $bag)
+                            Medio de Trasporte:{{ $bag->ITINERARIO }} = {{ $bag->TRASPORTE }}
+                        @endforeach
+                    </th>
                 </tr>
-                @php $i++; @endphp <!-- Incrementa $i en cada iteración -->
-            @endforeach
-            {{-- @foreach ($bags as $bag)
-                @php
-                    $nrosaca_number = (int) $bag->NROSACA + 1;
-                    $pesom_shown = false; // Variable para controlar si $bag->PESOM ya ha sido mostrado
-                    $pesor_shown = false; // Variable para controlar si $bag->PESOR ya ha sido mostrado
-                @endphp
-
-                @for ($j = 0; $j < $bag->SACAR; $j++)
+                <tr>
+                    <td style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
+                        <p>Fecha: {{ now()->format('Y-m-d H:i') }}</p>
+                    </td>
+                    <td style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
+                        @foreach ($bags->take(1) as $bag)
+                            Observaciones:{{ $bag->OBSERVACIONES }}
+                        @endforeach
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <table class="first-table">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Despacho</th>
+                    <th>Origen</th>
+                    <th>Destino</th>
+                    <th>Correspondencia</th>
+                    <th>Peso (Kg.)</th>
+                    <th>Observaciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php $i = 1; @endphp <!-- Inicializa $i con 1 -->
+                @foreach ($bags as $bag)
                     <tr>
                         <td>{{ $i }}</td>
-                        <td>{{ $bag->NRODESPACHO }} / {{ str_pad($nrosaca_number, 4, '0', STR_PAD_LEFT) }}</td>
+                        <td>{{ $bag->NRODESPACHO }} / {{ $bag->NROSACA }}{{ $bag->FIN == 'F' ? 'F' : '' }}</td>
                         <td>{{ $bag->OFCAMBIO }}</td>
                         <td>{{ $bag->OFDESTINO }}</td>
-                        <td>1 CERTFICADO</td>
-                        <td>{{ $pesor_shown ? '' : $bag->PESOR }}</td>
-                        <!-- Solo mostrar si $bag->PESOR no ha sido mostrado antes -->
+                        <td>
+                            @switch($bag->TIPO)
+                                @case('U')
+                                    1 ORDINARIO
+                                @break
+
+                                @case('M')
+                                    1 SACA M
+                                @break
+
+                                @case('R')
+                                    1 CERTIFICADO
+                                @break
+
+                                @default
+                                    Desconocido
+                            @endswitch
+                        </td>
+                        <td>{{ $bag->PESOF }}</td>
                         <td>{{ $bag->OBSERVACIONES }}</td>
                     </tr>
-                    @php
-                        if (!$pesor_shown) {
-                            $pesor_shown = true; // Marcar $bag->PESOR como mostrado
-                        }
-                        if (!$pesom_shown) {
-                            $pesom_shown = false; // Marcar $bag->PESOM como mostrado
-                        }
-                        $nrosaca_number++;
-                        $i++;
-                    @endphp
-                @endfor
-
-                @php
-                    $nrosaca_number = (int) $bag->NROSACA;
-                @endphp
-
-                @for ($k = 0; $k < $bag->SACAM; $k++)
+                    @php $i++; @endphp <!-- Incrementa $i en cada iteración -->
+                @endforeach
+                <!-- Filas vacías para llenar la página -->
+                @for ($j = $i; $j <= 30; $j++)
                     <tr>
-                        <td>{{ $i }}</td>
-                        <td>{{ $bag->NRODESPACHO }} / {{ str_pad($nrosaca_number, 4, '0', STR_PAD_LEFT) }}</td>
-                        <td>{{ $bag->OFCAMBIO }}</td>
-                        <td>{{ $bag->OFDESTINO }}</td>
-                        <td>1 SACA M</td>
-                        <td>{{ $pesom_shown ? '' : $bag->PESOM }}</td>
-                        <!-- Solo mostrar si $bag->PESOM no ha sido mostrado antes -->
-                        <td>{{ $bag->OBSERVACIONES }}</td>
+                        <td>{{ $j }}</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
                     </tr>
-                    @php
-                        if (!$pesor_shown) {
-                            $pesor_shown = true; // Marcar $bag->PESOR como mostrado
-                        }
-                        if (!$pesom_shown) {
-                            $pesom_shown = true; // Marcar $bag->PESOM como mostrado
-                        }
-                        $nrosaca_number++;
-                        $i++;
-                    @endphp
                 @endfor
-            @endforeach --}}
-        </tbody>
-    </table>
-    <table>
-        <thead>
-            <tr>
-                <td>SACAS TOTAL</td>
-                <th>{{ $sum->sum_paquetes }}</th>
-                <td>PESO TOTAL</td>
-                <th>{{ $sum->sum_pesoc }} Kg.</th>
-                <td>PAQUETES TOTAL</td>
-                <th>{{ $sum->sum_totalpaquetes }}</th>
-            </tr>
-        </thead>
-    </table>
-    <br>
-    <br>
-    <br>
-    <br>
-    <table>
-        <td style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
-            <p class="special-text">__________________________</p>
-            <p class="special-text">RECIBIDO POR</p>
-            <p class="special-text">{{ $bag->TRASPORTE }}</p>
-        </td>
-        <td style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
-            <p class="special-text">__________________________ </p>
-            <p class="special-text">ENTREGADO POR</p>
-            <p class="special-text">{{ auth()->user()->name }}</p>
-        </td>
-    </table>
+            </tbody>
+        </table>
+        <table>
+            <thead>
+                <tr>
+                    <td>SACAS TOTAL</td>
+                    <th>{{ $sum->sum_paquetes }}</th>
+                    <td>PESO TOTAL</td>
+                    <th>{{ $sum->sum_pesoc }} Kg.</th>
+                    <td>PAQUETES TOTAL</td>
+                    <th>{{ $sum->sum_totalpaquetes }}</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+    <div class="footer">
+        <table>
+            <td style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
+                <p class="special-text">__________________________</p>
+                <p class="special-text">RECIBIDO POR</p>
+                <p class="special-text">{{ $bag->TRASPORTE }}</p>
+            </td>
+            <td style="border: none; text-align: left; font-weight: normal; line-height: 0.1;">
+                <p class="special-text">__________________________ </p>
+                <p class="special-text">ENTREGADO POR</p>
+                <p class="special-text">{{ auth()->user()->name }}</p>
+            </td>
+        </table>
+    </div>
 </body>
 
 </html>
