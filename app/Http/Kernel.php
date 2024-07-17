a2d002c22038dd46c1d079c68d670fb891807a74
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-07-17 10:01:23 -0400

    jwt

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,7 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $middlewareAliases = [
+        'predefined.token' => \App\Http\Middleware\ValidatePredefinedToken::class,
         'cors' => \App\Http\Middleware\Cors::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/prueba/{codigo}', [ApiController::class, 'show']);
-Route::post('/ventanilla', [ApiController::class, 'ventanilla']);
+Route::middleware('predefined.token')->group(function () {
+    Route::get('/prueba/{codigo}', [ApiController::class, 'show']);
+    Route::post('/ventanilla', [ApiController::class, 'ventanilla']);
+});
