0204e6edba99eecf71969ebee9601a9c4b58746f
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-07-19 17:34:39 -0400

    Reapply "Merge branch 'dev'"
    This reverts commit 87e71910a94df2975beb79770b7c2c4b90a6f84d.

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,17 +7,248 @@ use App\Models\Package;
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade\Pdf;
 
-/**
- * Class EventController
- * @package App\Http\Controllers
- */
 class EventController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    // Lista de códigos de país y sus traducciones
+    private $countryCodes = [
+        'AF' => 'AFGHANISTAN',
+        'AL' => 'ALBANIA',
+        'DZ' => 'ALGERIA',
+        'AS' => 'AMERICAN SAMOA',
+        'AD' => 'ANDORRA',
+        'AO' => 'ANGOLA',
+        'AQ' => 'ANTARCTICA',
+        'AG' => 'ANTIGUA AND BARBUDA',
+        'AR' => 'ARGENTINA',
+        'AM' => 'ARMENIA',
+        'AW' => 'ARUBA',
+        'AU' => 'AUSTRALIA',
+        'AT' => 'AUSTRIA',
+        'AZ' => 'AZERBAIJAN',
+        'BS' => 'BAHAMAS',
+        'BH' => 'BAHRAIN',
+        'BD' => 'BANGLADESH',
+        'BB' => 'BARBADOS',
+        'BY' => 'BELARUS',
+        'BE' => 'BELGIUM',
+        'BZ' => 'BELIZE',
+        'BJ' => 'BENIN',
+        'BM' => 'BERMUDA',
+        'BT' => 'BHUTAN',
+        'BO' => 'BOLIVIA',
+        'BA' => 'BOSNIA AND HERZEGOVINA',
+        'BW' => 'BOTSWANA',
+        'BV' => 'BOUVET ISLAND',
+        'BR' => 'BRAZIL',
+        'IO' => 'BRITISH INDIAN OCEAN TERRITORY',
+        'BN' => 'BRUNEI DARUSSALAM',
+        'BG' => 'BULGARIA',
+        'BF' => 'BURKINA FASO',
+        'BI' => 'BURUNDI',
+        'KH' => 'CAMBODIA',
+        'CM' => 'CAMEROON',
+        'CA' => 'CANADA',
+        'CV' => 'CAPE VERDE',
+        'KY' => 'CAYMAN ISLANDS',
+        'CF' => 'CENTRAL AFRICAN REPUBLIC',
+        'TD' => 'CHAD',
+        'CL' => 'CHILE',
+        'CN' => 'CHINA',
+        'CX' => 'CHRISTMAS ISLAND',
+        'CC' => 'COCOS (KEELING) ISLANDS',
+        'CO' => 'COLOMBIA',
+        'KM' => 'COMOROS',
+        'CG' => 'CONGO',
+        'CD' => 'CONGO, THE DEMOCRATIC REPUBLIC OF THE',
+        'CK' => 'COOK ISLANDS',
+        'CR' => 'COSTA RICA',
+        'CI' => "CÔTE D'IVOIRE",
+        'HR' => 'CROATIA',
+        'CU' => 'CUBA',
+        'CY' => 'CYPRUS',
+        'CZ' => 'CZECH REPUBLIC',
+        'DK' => 'DENMARK',
+        'DJ' => 'DJIBOUTI',
+        'DM' => 'DOMINICA',
+        'DO' => 'DOMINICAN REPUBLIC',
+        'EC' => 'ECUADOR',
+        'EG' => 'EGYPT',
+        'SV' => 'EL SALVADOR',
+        'GQ' => 'EQUATORIAL GUINEA',
+        'ER' => 'ERITREA',
+        'EE' => 'ESTONIA',
+        'ET' => 'ETHIOPIA',
+        'FK' => 'FALKLAND ISLANDS (MALVINAS)',
+        'FO' => 'FAROE ISLANDS',
+        'FJ' => 'FIJI',
+        'FI' => 'FINLAND',
+        'FR' => 'FRANCE',
+        'GF' => 'FRENCH GUIANA',
+        'PF' => 'FRENCH POLYNESIA',
+        'TF' => 'FRENCH SOUTHERN TERRITORIES',
+        'GA' => 'GABON',
+        'GM' => 'GAMBIA',
+        'GE' => 'GEORGIA',
+        'DE' => 'GERMANY',
+        'GH' => 'GHANA',
+        'GI' => 'GIBRALTAR',
+        'GR' => 'GREECE',
+        'GL' => 'GREENLAND',
+        'GD' => 'GRENADA',
+        'GP' => 'GUADELOUPE',
+        'GU' => 'GUAM',
+        'GT' => 'GUATEMALA',
+        'GN' => 'GUINEA',
+        'GW' => 'GUINEA-BISSAU',
+        'GY' => 'GUYANA',
+        'HT' => 'HAITI',
+        'HM' => 'HEARD ISLAND AND MCDONALD ISLANDS',
+        'HN' => 'HONDURAS',
+        'HK' => 'HONG KONG',
+        'HU' => 'HUNGARY',
+        'IS' => 'ICELAND',
+        'IN' => 'INDIA',
+        'ID' => 'INDONESIA',
+        'IR' => 'IRAN, ISLAMIC REPUBLIC OF',
+        'IQ' => 'IRAQ',
+        'IE' => 'IRELAND',
+        'IL' => 'ISRAEL',
+        'IT' => 'ITALY',
+        'JM' => 'JAMAICA',
+        'JP' => 'JAPAN',
+        'JO' => 'JORDAN',
+        'KZ' => 'KAZAKHSTAN',
+        'KE' => 'KENYA',
+        'KI' => 'KIRIBATI',
+        'KP' => 'KOREA, DEMOCRATIC PEOPLE\'S REPUBLIC OF',
+        'KR' => 'KOREA, REPUBLIC OF',
+        'KW' => 'KUWAIT',
+        'KG' => 'KYRGYZSTAN',
+        'LA' => 'LAO PEOPLE\'S DEMOCRATIC REPUBLIC',
+        'LV' => 'LATVIA',
+        'LB' => 'LEBANON',
+        'LS' => 'LESOTHO',
+        'LR' => 'LIBERIA',
+        'LY' => 'LIBYAN ARAB JAMAHIRIYA',
+        'LI' => 'LIECHTENSTEIN',
+        'LT' => 'LITHUANIA',
+        'LU' => 'LUXEMBOURG',
+        'MO' => 'MACAO',
+        'MK' => 'MACEDONIA, THE FORMER YUGOSLAV REPUBLIC OF',
+        'MG' => 'MADAGASCAR',
+        'MW' => 'MALAWI',
+        'MY' => 'MALAYSIA',
+        'MV' => 'MALDIVES',
+        'ML' => 'MALI',
+        'MT' => 'MALTA',
+        'MH' => 'MARSHALL ISLANDS',
+        'MQ' => 'MARTINIQUE',
+        'MR' => 'MAURITANIA',
+        'MU' => 'MAURITIUS',
+        'YT' => 'MAYOTTE',
+        'MX' => 'MEXICO',
+        'FM' => 'MICRONESIA, FEDERATED STATES OF',
+        'MD' => 'MOLDOVA, REPUBLIC OF',
+        'MC' => 'MONACO',
+        'MN' => 'MONGOLIA',
+        'MS' => 'MONTSERRAT',
+        'MA' => 'MOROCCO',
+        'MZ' => 'MOZAMBIQUE',
+        'MM' => 'MYANMAR',
+        'NA' => 'NAMIBIA',
+        'NR' => 'NAURU',
+        'NP' => 'NEPAL',
+        'NL' => 'NETHERLANDS',
+        'AN' => 'NETHERLANDS ANTILLES',
+        'NC' => 'NEW CALEDONIA',
+        'NZ' => 'NEW ZEALAND',
+        'NI' => 'NICARAGUA',
+        'NE' => 'NIGER',
+        'NG' => 'NIGERIA',
+        'NU' => 'NIUE',
+        'NF' => 'NORFOLK ISLAND',
+        'MP' => 'NORTHERN MARIANA ISLANDS',
+        'NO' => 'NORWAY',
+        'OM' => 'OMAN',
+        'PK' => 'PAKISTAN',
+        'PW' => 'PALAU',
+        'PS' => 'PALESTINIAN TERRITORY, OCCUPIED',
+        'PA' => 'PANAMA',
+        'PG' => 'PAPUA NEW GUINEA',
+        'PY' => 'PARAGUAY',
+        'PE' => 'PERU',
+        'PH' => 'PHILIPPINES',
+        'PN' => 'PITCAIRN',
+        'PL' => 'POLAND',
+        'PR' => 'PUERTO RICO',
+        'QA' => 'QATAR',
+        'RE' => 'RÉUNION',
+        'RO' => 'ROMANIA',
+        'RU' => 'RUSSIAN FEDERATION',
+        'RW' => 'RWANDA',
+        'SH' => 'SAINT HELENA',
+        'KN' => 'SAINT KITTS AND NEVIS',
+        'LC' => 'SAINT LUCIA',
+        'PM' => 'SAINT PIERRE AND MIQUELON',
+        'VC' => 'SAINT VINCENT AND THE GRENADINES',
+        'WS' => 'SAMOA',
+        'SM' => 'SAN MARINO',
+        'ST' => 'SAO TOME AND PRINCIPE',
+        'SA' => 'SAUDI ARABIA',
+        'SN' => 'SENEGAL',
+        'CS' => 'SERBIA AND MONTENEGRO',
+        'SC' => 'SEYCHELLES',
+        'SL' => 'SIERRA LEONE',
+        'SG' => 'SINGAPORE',
+        'SK' => 'SLOVAKIA',
+        'SI' => 'SLOVENIA',
+        'SB' => 'SOLOMON ISLANDS',
+        'SO' => 'SOMALIA',
+        'ZA' => 'SOUTH AFRICA',
+        'GS' => 'SOUTH GEORGIA AND THE SOUTH SANDWICH ISLANDS',
+        'ES' => 'SPAIN',
+        'LK' => 'SRI LANKA',
+        'SD' => 'SUDAN',
+        'SR' => 'SURINAME',
+        'SJ' => 'SVALBARD AND JAN MAYEN',
+        'SZ' => 'SWAZILAND',
+        'SE' => 'SWEDEN',
+        'CH' => 'SWITZERLAND',
+        'SY' => 'SYRIAN ARAB REPUBLIC',
+        'TW' => 'TAIWAN, PROVINCE OF CHINA',
+        'TJ' => 'TAJIKISTAN',
+        'TZ' => 'TANZANIA, UNITED REPUBLIC OF',
+        'TH' => 'THAILAND',
+        'TL' => 'TIMOR-LESTE',
+        'TG' => 'TOGO',
+        'TK' => 'TOKELAU',
+        'TO' => 'TONGA',
+        'TT' => 'TRINIDAD AND TOBAGO',
+        'TN' => 'TUNISIA',
+        'TR' => 'TURKEY',
+        'TM' => 'TURKMENISTAN',
+        'TC' => 'TURKS AND CAICOS ISLANDS',
+        'TV' => 'TUVALU',
+        'UG' => 'UGANDA',
+        'UA' => 'UKRAINE',
+        'AE' => 'UNITED ARAB EMIRATES',
+        'GB' => 'UNITED KINGDOM',
+        'US' => 'UNITED STATES',
+        'UM' => 'UNITED STATES MINOR OUTLYING ISLANDS',
+        'UY' => 'URUGUAY',
+        'UZ' => 'UZBEKISTAN',
+        'VU' => 'VANUATU',
+        'VE' => 'VENEZUELA',
+        'VN' => 'VIET NAM',
+        'VG' => 'VIRGIN ISLANDS, BRITISH',
+        'VI' => 'VIRGIN ISLANDS, U.S.',
+        'WF' => 'WALLIS AND FUTUNA',
+        'EH' => 'WESTERN SAHARA',
+        'YE' => 'YEMEN',
+        'ZM' => 'ZAMBIA',
+        'ZW' => 'ZIMBABWE',
+    ];
+
     public function index()
     {
         $events = Event::paginate();
@@ -26,23 +257,12 @@ class EventController extends Controller
             ->with('i', (request()->input('page', 1) - 1) * $events->perPage());
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         $event = new Event();
         return view('event.create', compact('event'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         request()->validate(Event::$rules);
@@ -53,12 +273,6 @@ class EventController extends Controller
             ->with('success', 'Event created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $event = Event::find($id);
@@ -66,12 +280,6 @@ class EventController extends Controller
         return view('event.show', compact('event'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         $event = Event::find($id);
@@ -79,13 +287,6 @@ class EventController extends Controller
         return view('event.edit', compact('event'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  Event $event
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Event $event)
     {
         request()->validate(Event::$rules);
@@ -96,11 +297,6 @@ class EventController extends Controller
             ->with('success', 'Event updated successfully');
     }
 
-    /**
-     * @param int $id
-     * @return \Illuminate\Http\RedirectResponse
-     * @throws \Exception
-     */
     public function destroy($id)
     {
         $event = Event::find($id)->delete();
@@ -108,10 +304,18 @@ class EventController extends Controller
         return redirect()->route('events.index')
             ->with('success', 'Event deleted successfully');
     }
+
     public function search(Request $request)
     {
         $codigo = $request->input('codigo');
-        // Recupera todos los campos de la tabla 'packages'
+
+        // Extraer los últimos dos caracteres del código
+        $lastTwoChars = substr($codigo, -2);
+
+        // Obtener el nombre del país
+        $country = $this->countryCodes[$lastTwoChars] ?? 'Unknown Country';
+
+        // Realiza la consulta a la base de datos para obtener los paquetes
         $packages = Package::where('CODIGO', $codigo)
         ->take(5)
         ->withTrashed()
@@ -125,8 +329,40 @@ class EventController extends Controller
             ->orderBy('created_at', 'desc')
             ->get();
 
-            return view('search', compact('packages', 'event', 'codigo'));
+        // Consumir la API para autenticación y obtener el token JWT
+        $client = new Client(['base_uri' => 'http://172.65.10.37/']);
+        $response = $client->post('api/Autenticacion/Validar', [
+            'json' => [
+                'correo' => 'Correos',
+                'clave' => 'AGBClp2020'
+            ]
+        ]);
+        $body = json_decode($response->getBody());
+        $token = $body->token;
+
+        // Usar el token JWT para hacer la búsqueda en la otra API
+        $response = $client->post('api/O_MAIL_OBJECTS/buscar', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+                'Accept' => 'application/json',
+            ],
+            'json' => [
+                'id' => $codigo // Utilizando el valor del código
+            ]
+        ]);
+
+        // Decodificar y obtener los resultados de la búsqueda
+        $results = json_decode($response->getBody());
+
+        // Ordenar los resultados por fecha descendente
+        usort($results, function ($a, $b) {
+            return strtotime($b->eventDate) - strtotime($a->eventDate);
+        });
+
+        // Puedes devolver los resultados a tu vista junto con los paquetes y eventos
+        return view('search', compact('results', 'packages', 'event', 'codigo', 'country'));
     }
+
     public function eventspdf()
     {
         $events = Event::orderByDesc('created_at')->get();
