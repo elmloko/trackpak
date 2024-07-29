08033fe1e350ea257f85cd71c39654123119ebe5
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-07-29 17:57:48 -0400

    Update ApiController.php

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -25,11 +25,18 @@ class ApiController extends Controller
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
-    public function softdeletes()
+    public function softdeletes(Request $request)
     {
         try {
-            // Llamar al procedimiento almacenado
-            $packages = DB::select('CALL GetSoftDeletedPackages()');
+            // Parámetros de paginación
+            $perPage = $request->input('per_page', 15); // Cantidad de registros por página
+            $page = $request->input('page', 1); // Página actual
+
+            // Calcular el offset
+            $offset = ($page - 1) * $perPage;
+
+            // Llamar al procedimiento almacenado con limitación de registros
+            $packages = DB::select('CALL GetSoftDeletedPackagesLimited(?, ?)', [$offset, $perPage]);
 
             // Devolver los datos de los paquetes en formato JSON
             return response()->json($packages);
