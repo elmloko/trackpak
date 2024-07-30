08c439c384baebfb7a08653dc90c0aa9c656121d
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-07-30 15:21:37 -0400

    Update PlantillaeExport.php

--- app/Exports/PlantillaeExport.php
+++ app/Exports/PlantillaeExport.php
@@ -25,7 +25,6 @@ class PlantillaeExport implements FromCollection, WithHeadings, WithStyles
             'TELEFONO', 
             'PESO', 
             'ZONA', 
-            'TIPO'
         ];
     }
 
