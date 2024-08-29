2a542aa9ae09d6fab2b38b4ee62accee355c2bf7
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-08-29 16:24:14 -0400

    Update reencaminarentrega.blade.php

--- resources/views/livewire/pdf/reencaminarentrega.blade.php
+++ resources/views/livewire/pdf/reencaminarentrega.blade.php
@@ -73,6 +73,7 @@
                     <td>
                         <p class="small-text"><strong>Usuario:</strong> {{ auth()->user()->name }}</p>
                         <p class="small-text"><strong>Peso:</strong> {{ $package->PESO }} gr.</p>
+                        <p class="small-text"><strong>Ventanilla:</strong> {{ $package->TELEFONO }}</p>
                         <p class="small-text"><strong>Entrega:</strong> {{ $package->ESTADO }}</p>
                         <p class="small-text"><strong>Observaciones:</strong> {{ $package->OBSERVACIONES }}</p>
                         <p class="small-text"><strong>Fecha Entrega:</strong> {{ now()->format('Y-m-d H:i') }}</p>
@@ -86,6 +87,41 @@
                     <p class="special-text">RECIBIDO POR</p>
                     <p class="special-text"></p>
                 </td>
+            </table>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="modal-body">
+            <div class="logo">
+                <img src="{{ public_path('images/images.png') }}" alt="" width="100" height="50">
+            </div>
+            <div class="center-text">
+                <h2 class="normal-text" style="margin-top: 0;">FORMULARIO DE REENCAMINAMIENTO</h2>
+                <h3 class="normal-text">AGENCIA BOLIVIANA DE CORREOS</h3>
+            </div>
+            <table class="centro">
+                <tr>
+                    <td>
+                        <p class="barcode">{!! DNS1D::getBarcodeHTML($package->CODIGO, 'C128', 1.25, 25) !!}</p>
+                        <p class="small-text"><strong>Código Rastreo:</strong> {{ $package->CODIGO }}</p>
+                        <p class="small-text"><strong>Destinatario:</strong> {{ $package->DESTINATARIO }}</p>
+                        <p class="small-text"><strong>Ciudad:</strong> {{ auth()->user()->Regional }}</p>
+                        <p class="small-text"><strong>Rencaminado:</strong> {{ $package->CUIDAD }}</p>
+                        <p class="small-text"><strong>Ventanilla:</strong> {{ $package->VENTANILLA }}</p>
+                    </td>
+                    <td>
+                        <p class="small-text"><strong>Usuario:</strong> {{ auth()->user()->name }}</p>
+                        <p class="small-text"><strong>Peso:</strong> {{ $package->PESO }} gr.</p>
+                        <p class="small-text"><strong>Ventanilla:</strong> {{ $package->TELEFONO }}</p>
+                        <p class="small-text"><strong>Entrega:</strong> {{ $package->ESTADO }}</p>
+                        <p class="small-text"><strong>Observaciones:</strong> {{ $package->OBSERVACIONES }}</p>
+                        <p class="small-text"><strong>Fecha Entrega:</strong> {{ now()->format('Y-m-d H:i') }}</p>
+                    </td>
+                </tr>
+            </table>
+            <br>
+            <table>
                 <td>
                     <p class="special-text">__________________________ </p>
                     <p class="special-text">ENTREGADO POR</p>
