847e645dfe8a8a3387cfef214929d208aad93f27
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-10-04 12:40:54 -0400

    Cambios

--- app/Livewire/Traspasoventanillas.php
+++ app/Livewire/Traspasoventanillas.php
@@ -3,11 +3,24 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Package;
+use Livewire\WithPagination;
 
 class Traspasoventanillas extends Component
 {
+    use WithPagination;
+
     public function render()
     {
-        return view('livewire.traspasoventanillas');
+        // Filtrar paquetes donde el estado sea 'TRASPAZO'
+        $packages = Package::withTrashed()
+            ->where('ESTADO', 'TRASPAZO') // Filtrar por estado TRASPAZO
+            ->orderBy('created_at', 'desc')
+            ->paginate(10);
+
+        // Retorna la vista con los paquetes filtrados
+        return view('livewire.traspasoventanillas', [
+            'packages' => $packages,
+        ]);
     }
 }
