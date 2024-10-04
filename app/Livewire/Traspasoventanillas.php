da76c93da3c91765cebee4e415862634af75a8a3
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2024-10-04 12:46:38 -0400

    TRASPAZO VENTANILLA

--- app/Livewire/Traspasoventanillas.php
+++ app/Livewire/Traspasoventanillas.php
@@ -10,15 +10,34 @@ class Traspasoventanillas extends Component
 {
     use WithPagination;
 
+    public $searchTerm; // Variable para el código de búsqueda
+
+    public function searchPackage()
+    {
+        // Buscar el paquete por el código de rastreo y estado 'VENTANILLA'
+        $package = Package::where('CODIGO', $this->searchTerm)
+            ->where('ESTADO', 'VENTANILLA')
+            ->first();
+
+        if ($package) {
+            // Actualizar el estado del paquete a 'TRASPAZO'
+            $package->ESTADO = 'TRASPAZO';
+            $package->save();
+
+            session()->flash('message', 'El paquete con código ' . $this->searchTerm . ' ha sido actualizado a TRASPAZO.');
+        } else {
+            session()->flash('message', 'No se encontró ningún paquete con el código o el estado no es VENTANILLA.');
+        }
+    }
+
     public function render()
     {
-        // Filtrar paquetes donde el estado sea 'TRASPAZO'
-        $packages = Package::withTrashed()
-            ->where('ESTADO', 'TRASPAZO') // Filtrar por estado TRASPAZO
+        // Filtrar los paquetes con estado 'TRASPAZO' para mostrarlos en la tabla
+        $packages = Package::where('ESTADO', 'TRASPAZO')
             ->orderBy('created_at', 'desc')
             ->paginate(10);
 
-        // Retorna la vista con los paquetes filtrados
+        // Retornar la vista con los paquetes filtrados
         return view('livewire.traspasoventanillas', [
             'packages' => $packages,
         ]);
--- resources/views/livewire/traspasoventanillas.blade.php
+++ resources/views/livewire/traspasoventanillas.blade.php
@@ -3,81 +3,74 @@
         <div class="col-sm-12">
             <div class="card">
                 <div class="card-header" style="display: flex; justify-content: space-between; align-items: center;">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-sm-12">
-                                <div class="card">
-                                    <div>
-                                        <h5 id="card_title">
-                                            {{ __('Traspaso de Paquetes Ordinarios') }}
-                                        </h5>
-                                    </div>
-                                </div>
-                                <div class="card-body">
-                                    <div class="table-responsive">
-                                        @php
-                                            $i = 1; // Inicializa la variable $i
-                                        @endphp
-                                        @if ($packages->count())
-                                            <table class="table table-striped table-hover">
-                                                <thead class="thead">
-                                                    <tr>
-                                                        <th>No</th>
-                                                        <th>Código Rastreo</th>
-                                                        <th>Destinatario</th>
-                                                        <th>Teléfono</th>
-                                                        <th>País</th>
-                                                        <th>Ciudad</th>
-                                                        <th>Ventanilla</th>
-                                                        <th>Peso (gr.)</th>
-                                                        <th>Tipo</th>
-                                                        <th>Estado</th>
-                                                        <th>Aduana</th>
-                                                        <th>Observaciones</th>
-                                                        <th>Ultima Actualizacion</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    @foreach ($packages as $package)
-                                                        <tr>
-                                                            <td>{{ $i++ }}</td>
-                                                            <td>{{ $package->CODIGO }}</td>
-                                                            <td>{{ $package->DESTINATARIO }}</td>
-                                                            <td>{{ $package->TELEFONO }}</td>
-                                                            <td>{{ $package->PAIS }} - {{ $package->ISO }}</td>
-                                                            <td>{{ $package->CUIDAD }}</td>
-                                                            <td>{{ $package->VENTANILLA }}</td>
-                                                            <td>{{ $package->PESO }} </td>
-                                                            <td>{{ $package->TIPO }}</td>
-                                                            <td>
-                                                                @if($package->ESTADO == 'REPARTIDO')
-                                                                    ENTREGADO CARTERO
-                                                                @else
-                                                                    {{ $package->ESTADO }}
-                                                                @endif
-                                                            </td>
-                                                            <td>{{ $package->ADUANA }}</td>
-                                                            <td>{{ $package->OBSERVACIONES }}</td>
-                                                            <td>{{ $package->updated_at }}</td>
-                                                        </tr>
-                                                    @endforeach
-                                                </tbody>
-                                            </table>
-                                    </div>
-                                    <div class="row mt-3">
-                                        <div class="col-md-6">
-                                            {{ $packages->links() }}
-                                        </div>
-                                        <div class="col-md-6 text-right">
-                                            Se encontraron {{ $packages->total() }} registros en total
-                                        </div>
-                                    </div>
-                                @else
-                                    <p>No se encontraron resultados para la búsqueda.</p>
-                                    @endif
-                                </div>
+                    <h5 id="card_title">{{ __('Paquetes Ordinarios Nacionales') }}</h5>
+                </div>
+                <div class="card-body">
+                    <div class="mb-3">
+                        <!-- Formulario de búsqueda -->
+                        <input type="text" wire:model="searchTerm" class="form-control"
+                            placeholder="Buscar código de rastreo">
+                        <button wire:click="searchPackage" class="btn btn-primary mt-2">Buscar</button>
+                    </div>
+
+                    <div class="table-responsive">
+                        @if (session()->has('message'))
+                            <div class="alert alert-success">
+                                {{ session('message') }}
                             </div>
-                        </div>
+                        @endif
+
+                        @php
+                            $i = 1; // Inicializa la variable $i
+                        @endphp
+                        @if ($packages->count())
+                            <table class="table table-striped table-hover">
+                                <thead class="thead">
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Código Rastreo</th>
+                                        <th>Destinatario</th>
+                                        <th>Teléfono</th>
+                                        <th>País</th>
+                                        <th>Ciudad</th>
+                                        <th>Ventanilla</th>
+                                        <th>Peso (gr.)</th>
+                                        <th>Tipo</th>
+                                        <th>Estado</th>
+                                        <th>Aduana</th>
+                                        <th>Observaciones</th>
+                                        <th>Ultima Actualización</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($packages as $package)
+                                        <tr>
+                                            <td>{{ $i++ }}</td>
+                                            <td>{{ $package->CODIGO }}</td>
+                                            <td>{{ $package->DESTINATARIO }}</td>
+                                            <td>{{ $package->TELEFONO }}</td>
+                                            <td>{{ $package->PAIS }} - {{ $package->ISO }}</td>
+                                            <td>{{ $package->CUIDAD }}</td>
+                                            <td>{{ $package->VENTANILLA }}</td>
+                                            <td>{{ $package->PESO }} </td>
+                                            <td>{{ $package->TIPO }}</td>
+                                            <td>
+                                                @if ($package->ESTADO == 'REPARTIDO')
+                                                    ENTREGADO CARTERO
+                                                @else
+                                                    {{ $package->ESTADO }}
+                                                @endif
+                                            </td>
+                                            <td>{{ $package->ADUANA }}</td>
+                                            <td>{{ $package->OBSERVACIONES }}</td>
+                                            <td>{{ $package->updated_at }}</td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        @else
+                            <p>No se encontraron resultados para la búsqueda.</p>
+                        @endif
                     </div>
                 </div>
             </div>
