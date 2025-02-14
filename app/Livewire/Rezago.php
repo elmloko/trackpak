2136c8107aeb01af456df6d949d2cbe264f76518
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2025-02-14 16:50:02 -0400

    Cambios Rezago

--- app/Livewire/Rezago.php
+++ app/Livewire/Rezago.php
@@ -1,19 +1,19 @@
-<?php
+<?php 
 
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 use App\Models\Package;
 use App\Models\International;
-use Livewire\WithPagination;
+use App\Models\Event;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Exports\RezagoExport;
-use App\Models\Event;
 use Barryvdh\DomPDF\Facade\Pdf;
 
 class Rezago extends Component
 {
-    use WithPagination; // Mueve el uso de WithPagination aquí
+    use WithPagination;
 
     public $search = '';
     public $fecha_inicio;
@@ -21,15 +21,15 @@ class Rezago extends Component
 
     public function render()
     {
-        $packages = $this->getPackages();
-
         return view('livewire.rezago', [
-            'packages' => $packages,
+            'packages' => $this->getPackages(),
+            'internationals' => $this->getInternationals()
         ]);
     }
+
     public function getPackages()
     {
-        $packageQuery = Package::where('ESTADO', 'REZAGO')
+        return Package::where('ESTADO', 'REZAGO')
             ->when($this->search, function ($query) {
                 $query->where('CODIGO', 'like', '%' . $this->search . '%')
                     ->orWhere('DESTINATARIO', 'like', '%' . $this->search . '%')
@@ -38,57 +38,46 @@ class Rezago extends Component
                     ->orWhere('VENTANILLA', 'like', '%' . $this->search . '%')
                     ->orWhere('updated_at', 'like', '%' . $this->search . '%');
             })
-            ->select('id', 'CODIGO', 'DESTINATARIO', 'TELEFONO', 'CUIDAD', 'VENTANILLA', 'PESO', 'ESTADO', 'OBSERVACIONES', 'created_at');
-
-        $internationalQuery = International::where('ESTADO', 'REZAGO')
-            ->when($this->search, function ($query) {
-                $query->where('CODIGO', 'like', '%' . $this->search . '%')
-                    ->orWhere('DESTINATARIO', 'like', '%' . $this->search . '%')
-                    ->orWhere('TELEFONO', 'like', '%' . $this->search . '%')
-                    ->orWhere('CUIDAD', 'like', '%' . $this->search . '%')
-                    ->orWhere('VENTANILLA', 'like', '%' . $this->search . '%')
-                    ->orWhere('updated_at', 'like', '%' . $this->search . '%');
-            })
-            ->select('id', 'CODIGO', 'DESTINATARIO', 'TELEFONO', 'CUIDAD', 'VENTANILLA', 'PESO', 'ESTADO', 'OBSERVACIONES', 'created_at');
-
-        return $packageQuery->union($internationalQuery)
             ->orderBy('created_at', 'desc')
             ->paginate(100);
     }
+
+    public function getInternationals()
+    {
+        return International::where('ESTADO', 'REZAGO')
+            ->when($this->search, function ($query) {
+                $query->where('CODIGO', 'like', '%' . $this->search . '%')
+                    ->orWhere('DESTINATARIO', 'like', '%' . $this->search . '%')
+                    ->orWhere('TELEFONO', 'like', '%' . $this->search . '%')
+                    ->orWhere('CUIDAD', 'like', '%' . $this->search . '%')
+                    ->orWhere('VENTANILLA', 'like', '%' . $this->search . '%')
+                    ->orWhere('updated_at', 'like', '%' . $this->search . '%');
+            })
+            ->orderBy('created_at', 'desc')
+            ->paginate(50);
+    }
+
     public function export()
     {
         $this->validate([
             'fecha_inicio' => 'required|date',
             'fecha_fin' => 'required|date',
         ]);
-    
-        return Excel::download(new RezagoExport($this->fecha_inicio, $this->fecha_fin), 'Paquetes Rezagados.xlsx');
+
+        return Excel::download(new RezagoExport($this->fecha_inicio, $this->fecha_fin), 'Paquetes_Rezagados.xlsx');
     }
-    public function devolverPaquete($packageId)
+
+    public function devolverPaquete($packageId, $modelType)
     {
-        // Intentar encontrar el paquete en la tabla Package
-        $paquete = Package::find($packageId);
-        if ($paquete) {
-            $paquete->update([
-                'ESTADO' => 'PRE-REZAGO',
-                'updated_at' => now(),
-            ]);
-
-            Event::create([
-                'action' => 'DEVOLUCION',
-                'descripcion' => 'Devolución a URBANO',
-                'user_id' => auth()->user()->id,
-                'codigo' => $paquete->CODIGO,
-            ]);
-
-            session()->flash('success', 'El paquete ha sido devuelto a Ventanilla.');
-            return;
+        // Determinar si el paquete pertenece a Package o International
+        if ($modelType == 'Package') {
+            $package = Package::find($packageId);
+        } else {
+            $package = International::find($packageId);
         }
 
-        // Intentar encontrar el paquete en la tabla International
-        $paqueteInternacional = International::find($packageId);
-        if ($paqueteInternacional) {
-            $paqueteInternacional->update([
+        if ($package) {
+            $package->update([
                 'ESTADO' => 'PRE-REZAGO',
                 'updated_at' => now(),
             ]);
@@ -97,10 +86,13 @@ class Rezago extends Component
                 'action' => 'DEVOLUCION',
                 'descripcion' => 'Devolución a URBANO',
                 'user_id' => auth()->user()->id,
-                'codigo' => $paqueteInternacional->CODIGO,
+                'codigo' => $package->CODIGO,
             ]);
 
             session()->flash('success', 'El paquete ha sido devuelto a Ventanilla.');
+
+            // Refrescar los datos en la vista
+            $this->render();
         } else {
             session()->flash('error', 'Paquete no encontrado.');
         }
--- resources/views/livewire/rezago.blade.php
+++ resources/views/livewire/rezago.blade.php
@@ -3,110 +3,111 @@
         <div class="col-sm-12">
             <div class="card">
                 <div class="card-header" style="display: flex; justify-content: space-between; align-items: center;">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-md-12">
-                                <h5 id="card_title">
-                                    {{ __('Inventario de Paquetes en Rezagados en Ventanilla') }}
-                                </h5>
+                    <h5>{{ __('Inventario de Paquetes en Rezagados en Ventanilla') }}</h5>
+                </div>
+
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <label for="search">Buscar:</label>
+                                <input wire:model.lazy="search" type="text" class="form-control" placeholder="Buscar...">
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-lg-6">
-                                <div class="form-group">
-                                    <label for="search">Busca:</label>
-                                    <input wire:model.lazy="search" type="text" class="form-control" placeholder="Buscar...">
+                        <div class="col-lg-6">
+                            <form wire:submit.prevent="export" class="form-row align-items-center">
+                                <div class="col-md-4">
+                                    <label for="fecha_inicio">Fecha de inicio:</label>
+                                    <input type="date" wire:model="fecha_inicio" class="form-control" required>
                                 </div>
-                            </div>
-                            <div class="col-lg-6">
-                                <form wire:submit.prevent="export"
-                                    class="form-row align-items-center">
-                                    <div class="col-md-4">
-                                        <label for="fecha_inicio">Fecha de inicio:</label>
-                                        <input type="date" wire:model="fecha_inicio"
-                                            class="form-control" required>
-                                        @error('fecha_inicio')
-                                            <span class="text-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
-                                    <div class="col-md-4">
-                                        <label for="fecha_fin">Fecha de fin:</label>
-                                        <input type="date" wire:model="fecha_fin"
-                                            class="form-control" required>
-                                        @error('fecha_fin')
-                                            <span class="text-danger">{{ $message }}</span>
-                                        @enderror
-                                    </div>
-                                    <div class="col-md-4">
-                                        <button type="submit" class="btn btn-success">Generar
-                                            Excel</button>
-                                    </div>
-                                </form>
-                            </div>
+                                <div class="col-md-4">
+                                    <label for="fecha_fin">Fecha de fin:</label>
+                                    <input type="date" wire:model="fecha_fin" class="form-control" required>
+                                </div>
+                                <div class="col-md-4">
+                                    <button type="submit" class="btn btn-success">Generar Excel</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
+
                 @if ($message = Session::get('success'))
-                    <div class="alert alert-success">
-                        <p>{{ $message }}</p>
-                    </div>
+                    <div class="alert alert-success">{{ $message }}</div>
                 @endif
-                @if ($packages->count() > 0)
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <thead class="thead">
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Código Rastreo</th>
-                                        <th>Destinatario</th>
-                                        <th>Ciudad</th>
-                                        <th>Ventanilla</th>
-                                        <th>Peso</th>
-                                        <th>Estado</th>
-                                        <th>Observaciones</th>
-                                        <th>Fecha Rezago</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @php $i = 0; @endphp
-                                    @foreach ($packages as $package)
-                                        @if ($package->ESTADO !== 'DOMICILIO')
-                                            <tr>
-                                                <td>{{ ++$i }}</td>
-                                                <td>{{ $package->CODIGO }}</td>
-                                                <td>{{ $package->DESTINATARIO }}</td>
-                                                <td>{{ $package->CUIDAD }}</td>
-                                                <td>{{ $package->VENTANILLA }}</td>
-                                                <td>{{ $package->PESO }} gr.</td>
-                                                <td>{{ $package->ESTADO }}</td>
-                                                <td>{{ $package->OBSERVACIONES }}</td>
-                                                <td>{{ $package->created_at }}</td>
-                                                @hasrole('SuperAdmin|Administrador')
-                                                <td>
-                                                    <button wire:click="devolverPaquete({{ $package->id }})" class="btn btn-primary btn-sm">
-                                                        <i class="fas fa-undo-alt"></i> Devolver
-                                                    </button>
-                                                </td>
-                                                @endhasrole
-                                            </tr>
-                                        @endif
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="row mt-3">
-                            <div class="col-md-6">
-                                {{ $packages->links() }}
-                            </div>
-                            <div class="col-md-6 text-right">
-                                Se encontraron {{ $packages->total() }} registros en total
-                            </div>
-                        </div>
+
+                <div class="table-responsive">
+                    <table class="table table-striped table-hover">
+                        <thead class="thead">
+                            <tr>
+                                <th>No</th>
+                                <th>Código Rastreo</th>
+                                <th>Destinatario</th>
+                                <th>Ciudad</th>
+                                <th>Ventanilla</th>
+                                <th>Peso</th>
+                                <th>Estado</th>
+                                <th>Observaciones</th>
+                                <th>Fecha Rezago</th>
+                                @hasrole('SuperAdmin|Administrador')
+                                <th>Acciones</th>
+                                @endhasrole
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @php $i = 0; @endphp
+
+                            {{-- Listar paquetes nacionales --}}
+                            @foreach ($packages as $package)
+                                <tr>
+                                    <td>{{ ++$i }}</td>
+                                    <td>{{ $package->CODIGO }}</td>
+                                    <td>{{ $package->DESTINATARIO }}</td>
+                                    <td>{{ $package->CUIDAD }}</td>
+                                    <td>{{ $package->VENTANILLA }}</td>
+                                    <td>{{ $package->PESO }} gr.</td>
+                                    <td>{{ $package->ESTADO }}</td>
+                                    <td>{{ $package->OBSERVACIONES }}</td>
+                                    <td>{{ $package->created_at }}</td>
+                                    <td>
+                                        <button wire:click="devolverPaquete({{ $package->id }}, 'Package')" class="btn btn-primary btn-sm">
+                                            <i class="fas fa-undo-alt"></i> Devolver
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                            {{-- Listar paquetes internacionales --}}
+                            @foreach ($internationals as $international)
+                                <tr>
+                                    <td>{{ ++$i }}</td>
+                                    <td>{{ $international->CODIGO }}</td>
+                                    <td>{{ $international->DESTINATARIO }}</td>
+                                    <td>{{ $international->CUIDAD }}</td>
+                                    <td>{{ $international->VENTANILLA }}</td>
+                                    <td>{{ $international->PESO }} gr.</td>
+                                    <td>{{ $international->ESTADO }}</td>
+                                    <td>{{ $international->OBSERVACIONES }}</td>
+                                    <td>{{ $international->created_at }}</td>
+                                    <td>
+                                        <button wire:click="devolverPaquete({{ $international->id }}, 'International')" class="btn btn-primary btn-sm">
+                                            <i class="fas fa-undo-alt"></i> Devolver
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="row mt-3">
+                    <div class="col-md-6">
+                        {{ $packages->links() }}
                     </div>
-                @else
-                    <p>No hay elementos eliminados.</p>
-                @endif
+                    <div class="col-md-6 text-right">
+                        Se encontraron {{ $packages->total() + $internationals->total() }} registros en total
+                    </div>
+                </div>
             </div>
         </div>
     </div>
