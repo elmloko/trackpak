a8cb17b9070048580341911e3140e57eb0c409c0
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2025-05-22 14:28:45 -0400

    Revert "Cambios Captcha"
    This reverts commit 9dc0174835940ee56662491f392b24e947654b56.

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -311,7 +311,7 @@ class EventController extends Controller
     {
         $request->validate([
             'codigo' => 'required|size:13',
-            'captcha' => 'required|captcha',
+            'g-recaptcha-response' => 'required|captcha'
         ]);
 
         $codigo = $request->input('codigo');
