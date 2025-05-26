4fb3b73857b14d5a0763a3fa41b02ec28d99f1ec
Marco Antonio Espinoza Rojas <56237010+elmloko@users.noreply.github.com>
2025-05-26 16:05:48 -0400

    Revert "Fallo"
    This reverts commit b5714d7f3a5f8ea719a7300b1ae51e3b09613fd7.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,26 +32,5 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('viewPulse', function (?User $user) {
             return $user !== null;
         });
-
-        // 🔒 Agrega HttpOnly a XSRF-TOKEN (opcional, si NO lo usas con JavaScript)
-        app('router')->middleware('web')->group(function () {
-            app('events')->listen('Illuminate\Routing\Events\RouteMatched', function () {
-                $token = csrf_token();
-    
-                Cookie::queue(
-                    new SymfonyCookie(
-                        'XSRF-TOKEN',
-                        $token,
-                        now()->addMinutes(config('session.lifetime')),
-                        '/',
-                        null,
-                        config('session.secure'),
-                        true, // HttpOnly
-                        false,
-                        'Lax'
-                    )
-                );
-            });
-        });
     }
 }
