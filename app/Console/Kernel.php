f2972f9156b2f4e664a79ece551b3dbb0f454ce5
colee615 <94701865+colee615@users.noreply.github.com>
2026-02-10 16:20:19 -0400

    Revert "Cambios prueba Eynar"
    This reverts commit c44a27ace20f16406ac85c8d4fd7a5083545a47f.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,10 +10,10 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      */
-  protected function schedule(Schedule $schedule)
-{
-    $schedule->command('tracking:check')->everyTwoMinutes();
-}
+    protected function schedule(Schedule $schedule): void
+    {
+        // $schedule->command('inspire')->hourly();
+    }
 
     /**
      * Register the commands for the application.
